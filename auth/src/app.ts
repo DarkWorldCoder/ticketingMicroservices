0e1464ebefa92e28b6ac1c63b227ad328c1cf9b9
DarkWorldCoder <ayushniroula08@gmail.com>
2023-06-29 22:04:16 +0545

    yup

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -23,7 +23,7 @@ app.get("/",(req,res)=>{
     res.send("Hello World nowis the time")
 })
 app.get("/all",(req,res)=>{
-    console.log("hellso")
+    console.log("hellsdo")
     res.send("hello")
 })
 app.use(currentUserRouter)
